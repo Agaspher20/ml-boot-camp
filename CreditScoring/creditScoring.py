90622616c3d516523ee5a019b03f33927b84e15c
Michael Naumenko <agaspher@outlook.com>
2017-10-22 18:37:15 +0300

    built baseline model

--- CreditScoring/creditScoring.py
+++ CreditScoring/creditScoring.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_score
 
 sys.path.append("ml-boot-camp\\CreditScoring")
 import helperFunctions as hlp
@@ -48,26 +50,28 @@ def transform_columns_to_bool(train_frame, test_frame, indices):
     """ Преобразует список категориальных колонок в колонке, принимающие бинарные значения."""
     false_val = -1.
     true_val = 1.
+    train_frame_res = train_frame.copy()
+    test_frame_res = test_frame.copy()
     for col_index in indices:
-        col_values = list(train_frame[col_index]) + list(test_frame[col_index])
+        col_values = list(train_frame_res[col_index]) + list(test_frame_res[col_index])
         col_values = set(col_values)
         col_values.pop()
-        train_frame, next_true_val_train = transform_to_bool_columns(
-            train_frame,
+        train_frame_res, next_true_val_train = transform_to_bool_columns(
+            train_frame_res,
             col_index,
             col_values,
             false_value=false_val,
             init_true_value=true_val)
             
-        test_frame, next_true_val_test = transform_to_bool_columns(
-            test_frame,
+        test_frame_res, next_true_val_test = transform_to_bool_columns(
+            test_frame_res,
             col_index,
             col_values,
             false_value=false_val,
             init_true_value=true_val)
         false_val -= 1.
         true_val = next_true_val_train if next_true_val_train > next_true_val_test else next_true_val_test
-    return (train_frame, test_frame)
+    return (train_frame_res, test_frame_res)
 #%%
 # Загрузим тренировочные данные
 train_x = read_and_prepare_data("ml-boot-camp\\Data\\crx_data_train_x.csv")
@@ -86,8 +90,7 @@ hlp.frame_report(test_x)
 # Колонки с количеством значений 15 и меньше похожи на категориальные.
 # Выпишем категориальные индексы колонок
 categorial_columns = [0,3,4,5,6,8,9,11,12]
-train_x,test_x = transform_columns_to_bool(train_x, test_x, categorial_columns)
-
+train_x_base,test_x_base = transform_columns_to_bool(train_x, test_x, categorial_columns)
 # Поскольку одни и те же колонки являются категориальными и числовыми,
 # можно применить одни и те же трансформации, чтобы убрать пропуски в
 # данных    
@@ -95,10 +98,30 @@ train_x,test_x = transform_columns_to_bool(train_x, test_x, categorial_columns)
 # На самом деле колонку с 22 значениями можно попробовать проинтерпретировать как категориальную и
 # посмотреть, что получится.
 #%%
-train_x_y = train_x.copy()
+train_x_y = train_x_base.copy()
 train_x_y[15] = train_y[0]
 sns.pairplot(train_x_y)
 # По pairplot можно сделать следующие выводы
-# 1. Не наблюдается сильно связанных признаков
+# 1. Не наблюдается коллинеарных признаков
 # 2. Есть признаки неплохо описывающие ответ
 # 3. Классы в выборке распределены равномерно
+#%%
+# После препроцессинга попробуем обучить логистическую регрессию
+X = train_x_base.as_matrix()
+y = train_y.as_matrix().flatten()
+#%%
+base_line_model = LogisticRegression()
+scorer = cross_val_score(base_line_model, X, y, cv=10)
+print(scorer.mean())
+#%%
+def write_answer(fileName, answer):
+    with open("ml-boot-camp\\Results\\credit_scoring_" + fileName + ".csv", "w") as fout:
+        for val in answer:
+            fout.write(str(val)+"\n")
+#%%
+base_line_model = LogisticRegression()
+X_test = test_x_base.as_matrix()
+base_line_model.fit(X, y)
+base_line_result = base_line_model.predict(X_test)
+print(base_line_result)
+write_answer("baseLine", base_line_result)
